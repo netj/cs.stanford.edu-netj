158ac5592c330b5236ceee34f344cf429de03fab
Jaeho Shin <netj@cs.stanford.edu>
2013-05-31 01:39:16 -0700

    Google Analytics (UA-41346073-1)

--- update.sh
+++ update.sh
@@ -11,5 +11,7 @@ echo '<html><head><meta charset="utf-8"><title>'"$title"'</title>'
 cat HEADER.html
 echo '</head><body><section>'
 marked index.md
-echo '</section></body></html>'
+echo '</section>'
+cat FOOTER.html
+echo '</body></html>'
 } >$name.html
